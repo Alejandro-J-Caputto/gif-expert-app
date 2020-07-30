3004010d254f48f573bf42b3cf480ab9751dc956
Alejandro-J-Caputo <caputto.frias.alejandro@gmail.com>
2020-07-30 10:33:40 +0200

    build-docs

---
+++ docs/precache-manifest.ebef1e0be81e94a964e2f86d1fa6147d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d23ca60efddc74230ddca495edbd413",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c6543f0f47f6b9fa63d",
+    "url": "/static/css/main.e1c8be55.chunk.css"
+  },
+  {
+    "revision": "ef7fe5dcd6fe2f866af2",
+    "url": "/static/js/2.c555a145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c555a145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6543f0f47f6b9fa63d",
+    "url": "/static/js/main.e7b02c9e.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
